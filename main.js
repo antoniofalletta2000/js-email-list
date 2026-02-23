12979f989ee498d8c660bbc3ee0b14c0df9c457b
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-02-23 14:51:37 +0100

    complete exercise

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-const endpoint="https://flynn.boolean.careers/exercises/api/random/mail"
+const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail"
+const emailListEl = document.getElementById("email_list")
+
+for (let index = 0; index < 10; index++) {
+    fetch(endpoint)
+    .then(response => response.json())
+    .then((data) => {
+        const { response } = data
+        const markup = `<li>${response}</li>`
+        console.log(markup);
+        emailListEl.innerHTML+=markup
+    })
+}
+
